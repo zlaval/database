8c859bf7a2dd15c0c8c0db1d829af74de1cca00c
Zalan Toth <toth.zalan.ext@nokia.com>
2017-08-28 13:15:47 +0200

    add liquibase migration files

--- src/main/java/com/zlrx/database/domain/ProgrammingLanguage.java
+++ src/main/java/com/zlrx/database/domain/ProgrammingLanguage.java
@@ -12,7 +12,7 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @Entity
-@Table
+@Table(name = "programming_language")
 public class ProgrammingLanguage extends BaseEntity {
 
     @Basic
--- src/test/java/com/zlrx/database/DatabaseApplicationTests.java
+++ src/test/java/com/zlrx/database/DatabaseApplicationTests.java
@@ -1,7 +1,9 @@
 package com.zlrx.database;
 
+import com.zlrx.database.repository.ProgrammerRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,14 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DatabaseApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private ProgrammerRepository programmerRepository;
+
+    @Test
+    public void loadProgrammer() {
+
+
+    }
+
 
 }
